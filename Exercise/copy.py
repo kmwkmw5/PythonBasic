97cbf64b5da800a79194b4b7ab5fa4e0e8d927bc
kmwkmw5 <kmwkmw5@naver.com>
2014-01-19 23:21:11 +0900

    exercise copy, car calss

---
+++ Exercise/copy.py
@@ -0,0 +1,24 @@
+import sys;
+
+try:
+	originFileName = sys.argv[1];
+	copyFileName = sys.argv[2];
+except:
+	print('Usage: python copy.py originFileName copyFileName')
+	exit()
+
+try:
+	originFile = open(originFileName, 'r')
+except:
+	print(originFileName + ' open error!')
+	exit()
+data = originFile.read();
+originFile.close()
+
+try:
+	copyFile = open(copyFileName, 'w')
+except:
+	print(copyFileName + ' open error!')
+	exit()
+copyFile.write(data)
+copyFile.close()
